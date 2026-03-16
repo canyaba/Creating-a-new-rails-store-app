65cfdc2daf8c60b9796b8b092f01cee259f2522f
canyaba <canyaba.rrc.ca>
2026-03-16 00:16:56 -0500

    Add product validations and fix Rails console boot on Windows

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,15 @@
+require "rubygems"
+
+# Ruby 4 on Windows may install fiddle outside the app bundle, but IRB/Reline
+# still needs it for `rails console`.
+if Gem.win_platform? && Gem::Version.new(RUBY_VERSION) >= Gem::Version.new("4.0.0")
+  fiddle_lib = Dir[File.join(Gem.default_dir, "gems", "fiddle-*", "lib")].sort.last
+  fiddle_ext = Dir[File.join(Gem.default_dir, "extensions", "**", "fiddle-*")].sort.last
+
+  $LOAD_PATH.unshift(fiddle_lib) if fiddle_lib && !$LOAD_PATH.include?(fiddle_lib)
+  $LOAD_PATH.unshift(fiddle_ext) if fiddle_ext && !$LOAD_PATH.include?(fiddle_ext)
+end
+
 ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
